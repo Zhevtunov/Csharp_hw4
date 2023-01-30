3882287ebee7cdb96f780e9ff337f3e4cccfa503
Alexandr <A_L_E_X1987@list.ru>
2023-01-30 22:49:46 +0300

    задачи

---
+++ task1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+
+void GetDegree(int numberA, int numberB)
+{
+  int rez = 1;
+  for (int i = 1; i <= numberB; i++)
+  {
+    rez = rez * numberA;
+  }
+  Console.WriteLine(numberA + " в степени " + numberB + " получится " + rez);
+}
+
+int GetNum(string text)
+{
+  Console.Write(text);
+  return int.Parse(Console.ReadLine());
+}
+
+int numA = GetNum("Введите число : ");
+int numB = GetNum("В какую степень возвести это число ?: ");
+
+GetDegree(numA, numB);
---
+++ task2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число
+// и выдаёт сумму цифр в числе.
+
+void SumNumbers(int num)
+{
+  int print = num;
+  int count = 0;
+  int rez = 0;
+  while (num != 0)
+  {
+    count = num % 10;
+    num = num / 10;
+    rez = rez + count;
+  }
+  Console.WriteLine(" сумма всех цифр (" +print+ ") что вы ввели = " + rez);
+}
+
+int GetNum(string text)
+{
+  Console.Write(text);
+  return int.Parse(Console.ReadLine());
+}
+
+int num = GetNum("Введите число: ");
+
+SumNumbers(num);
